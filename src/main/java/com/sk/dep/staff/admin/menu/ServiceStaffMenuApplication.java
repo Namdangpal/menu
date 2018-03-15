8ce4c6a395d4eba00d74b88dba2f251ec65e5866
Administrator <Administrator@SKCC14D00588.SKCC.NET>
2018-03-15 18:33:17 +0900

    POM수정

--- src/main/java/com/sk/dep/staff/admin/menu/ServiceStaffMenuApplication.java
+++ src/main/java/com/sk/dep/staff/admin/menu/ServiceStaffMenuApplication.java
@@ -2,8 +2,12 @@ package com.sk.dep.staff.admin.menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
+
 public class ServiceStaffMenuApplication {
 
 	public static void main(String[] args) {
